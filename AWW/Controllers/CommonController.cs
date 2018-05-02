974ef6d3da34c0fe43a288b0041e4ec9f5e3f6e5
M. Salah <eng.msalah.abdullah@gmail.com>
2018-05-02 18:33:56 +0400

    Add project files.

---
+++ AWW/Controllers/CommonController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using AWW.Infrastructure.Helpers;
+namespace AWW.Controllers
+{
+    public class CommonController : FrontBaseController
+    {
+        
+        public CommonController()
+        {
+            
+        }
+        // GET: Common
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                //cookie.Expires = DateTime.Now.AddTicks(1);
+            }
+            Response.Cookies.Add(cookie);
+            //Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(culture);
+            //Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+
+           System.Web.HttpContext.Current.Session["culture"] = culture;
+
+           // return RedirectToAction("Index","Home");
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+        [ChildActionOnly]
+        public ActionResult NewsletterWidget()
+        {
+            return PartialView("_NewsletterWidget");
+        }
+        [HttpPost]
+        public ActionResult SubscribeNewsletter(string email, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                ModelState.Clear();
+
+            }
+
+            
+
+            return Redirect(returnUrl);
+           
+
+        }
+
+    }
+}
---
+++ AWW/Models/SubscribersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AWW.Models
+{
+    public class SubscribersViewModel
+    {
+        public int Id { get; set; }
+         [Required]
+        public string Email { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}
